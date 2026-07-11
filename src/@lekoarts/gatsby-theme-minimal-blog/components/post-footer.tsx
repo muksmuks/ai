8ce8c6dccb2bfec9494e73780cbf4fd4e053ae63
muksmuks <mukesh.kumar43585@gmail.com>
2026-07-11 22:13:05 +0530

    page redesign

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post-footer.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import type { MBPostProps } from "@lekoarts/gatsby-theme-minimal-blog/src/components/post"
+import Utterances from "../../../components/utterances"
+
+const PostFooter = (_props: MBPostProps) => <Utterances />
+
+export default PostFooter
